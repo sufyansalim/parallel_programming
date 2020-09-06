601ed0ea9b6068f8c0fb6673efd58dccf0c854b1
Sufyan Saleem <zslayer055@gmail.com>
2020-09-06 12:15:50 -0700

    first commit

---
+++ call_from.cpp
@@ -0,0 +1,32 @@
+//g++ call_from.cpp -lpthread -o call_from.exe
+#include <iostream>
+#include <thread>
+
+using std::cout;
+using std::endl;
+using std::thread;
+
+// Initial function for each thread
+void call_from(int tid) {
+  cout << "Launched by thread " << tid << endl;
+}
+
+int main() {
+  // Create an array of 10 thread objects
+  thread t[10];
+
+  // Launch 10 threads with initial function "call_from(...)"
+  for (int i = 0; i < 10; i++) {
+    t[i] = thread(call_from, i);
+  }
+
+  // Print from main thread
+  call_from(10);
+
+  // Wait for all threads to complete (in order)
+  for (int i = 0; i < 10; i++) {
+    t[i].join();
+  }
+
+  return 0;
+}
